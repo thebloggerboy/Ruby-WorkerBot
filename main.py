3e963dc5377f6e45ee99f883dd6ee49c2d9bbf42
thebloggerboy <thebloggerboy06@gmail.com>
2025-07-17 23:39:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from handlers import setup_handlers
 
 load_dotenv()
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-logger = logging.getLogger(__name__)
-TOKEN = os.environ.get("WORKER_BOT_TOKEN") # <-- ध्यान दें: WORKER_BOT_TOKEN
+TOKEN = os.environ.get("WORKER_BOT_TOKEN")
 
 app = Flask('')
 @app.route('/')
@@ -18,7 +17,7 @@ def run_flask(): app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 def keep_alive(): Thread(target=run_flask).start()
 
 def main():
-    if not TOKEN: logger.critical("WORKER_BOT_TOKEN not set!"); return
+    if not TOKEN: logging.critical("WORKER_BOT_TOKEN not set!"); return
     application = Application.builder().token(TOKEN).build()
     setup_handlers(application)
     keep_alive()
